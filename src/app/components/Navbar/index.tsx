61fd2543312485880be20c61d19bab29d40f84a2
Guilherme <guilherme.dds.dev@gmail.com>
2026-03-27 15:14:50 -0300

    style: modifica navbar de acordo com a página

--- src/app/components/Navbar/index.tsx
+++ src/app/components/Navbar/index.tsx
@@ -1,14 +1,19 @@
+"use client"
+
 import "./index.css"
 import { FaSearch } from "react-icons/fa";
+import Link from "next/link"
+import { usePathname } from "next/navigation";
 
 export default function Navbar() {
+  const pathname = usePathname()
     return (
     <nav className="navbar">
         <h1 className="page-title">Filmes</h1>
         <ul>
-          <li className="movies-btn">Filmes</li>
-          <li className="shows-btn">Séries</li>
-          <li className="list-btn">Lista</li>
+          <Link href="/" className={pathname == "/" ? "active-nav" : "default-nav"}>Filmes</Link>
+          <Link href="/Series" className={pathname == "/Series" ? "active-nav" : "default-nav"}>Séries</Link>
+          <Link href="/List" className={pathname == "/List" ? "active-nav" : "default-nav"}>Lista</Link>
         </ul>
         <div className="search-btn">
           <FaSearch color="white" size={20} />
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,6 +4,10 @@ import MovieList from "./components/MovieList";
 export default function Home() {
   return (
     <div>
+      <div className="header">
+        <img src="/avengers-endgame-banner.png" alt="" />
+      </div>
+      
       <MovieList />
     </div>
   );
