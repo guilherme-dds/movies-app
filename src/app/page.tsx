ba9bcd3f485dd1b4dc6ff0e79b8d23a86579fcef
Guilherme <guilherme.dds.dev@gmail.com>
2026-03-27 20:42:49 -0300

    feat: adiciona informações header

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,45 @@
 import MovieList from "./components/MovieList";
-import styles from "./page.module.css"
+import styles from "./page.module.css";
+import { FaPlay, FaPlus } from "react-icons/fa";
+import { AiOutlineLike } from "react-icons/ai";
 
 export default function Home() {
   return (
     <div>
       <div className={styles.header}>
-        <img src="/avengers-endgame-banner.png" style={{ borderRadius: "20px" }} alt="" />
+        <div className={styles.headerContent}>
+          <h1>Vingadores: Ultimato</h1>
+          <p>
+            With the help of remaining allies, the Avengers must assemble once
+            more in order to undo Thanos's actions and undo the chaos to the
+            universe, no matter what consequences may be in store, and no matter
+            who they face... Avenge the fallen.
+          </p>
+          <div className={styles.buttons}>
+            <div className={styles.playBtn}>
+              <FaPlay />
+              <span>Play Trailer</span>
+            </div>
+            <button>
+              <FaPlus />
+            </button>
+            <button>
+              <AiOutlineLike />
+            </button>
+          </div>
+        </div>
+        <img
+          src="/avengers-endgame-banner.png"
+          style={{ borderRadius: "20px" }}
+          alt=""
+          className={styles.headerImg}
+        />
       </div>
-      
+
+      <div className={styles.genres}>
+        <div className={styles.title}>Gêneros</div>
+      </div>
+
       <MovieList />
     </div>
   );
