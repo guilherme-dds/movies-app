376a89edd92661f0c9e618992cfc27ce66e038ce
Guilherme <guilherme.dds.dev@gmail.com>
2026-03-27 22:20:11 -0300

    feat: adiciona a busca dos filmes por gênero

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,53 @@
+"use client";
+
 import MovieList from "./components/MovieList";
 import styles from "./page.module.css";
 import { FaPlay, FaPlus, FaArrowLeft, FaArrowRight } from "react-icons/fa";
 import { AiOutlineLike } from "react-icons/ai";
+import { useEffect, useState } from "react";
+import axios from "axios";
+import { Movie } from "@/types/movie";
+import { Params } from "@/types/params";
 
 export default function Home() {
+  const [movies, setMovies] = useState<Movie[]>([]);
+  const [actionMovies, setActionMovies] = useState<Movie[]>([]);
+
+  const API_URL = "https://api.themoviedb.org/3/discover/movie";
+  const apiKey: string = process.env.NEXT_PUBLIC_TMDB_API_KEY!;
+
+  const IMAGE_BASE_URL = "https://image.tmdb.org/t/p/w500";
+
+  const getMovies = (movieType: string) => {
+    let paramsInfo: Params = {
+      api_key: apiKey,
+      language: "pt-BR",
+    };
+
+    if (movieType == "action") {
+      paramsInfo.with_genres = 28;
+    }
+
+    axios({
+      method: "get",
+      url: API_URL,
+      params: paramsInfo,
+    }).then((response) => {
+      const data = response.data.results.slice(0, 4);
+      console.log(data);
+      if (movieType == "action") {
+        setActionMovies(data);
+      } else {
+        setMovies(data);
+      }
+    });
+  };
+
+  useEffect(() => {
+    getMovies("action");
+    getMovies("");
+  }, []);
+
   return (
     <div>
       <div className={styles.header}>
@@ -54,6 +98,17 @@ export default function Home() {
 
       <div className={styles.genres}>
         <div className={styles.title}>Gêneros</div>
+        <div className={styles.genresRow}>
+          <div className={styles.genresSection}>
+            {actionMovies.map((actionMovie) => (
+              <img src={`${IMAGE_BASE_URL}${actionMovie.poster_path}`} alt="" />
+            ))}
+          </div>
+          <div className={styles.genresSection}></div>
+          <div className={styles.genresSection}></div>
+          <div className={styles.genresSection}></div>
+          <div className={styles.genresSection}></div>
+        </div>
       </div>
 
       <MovieList />
